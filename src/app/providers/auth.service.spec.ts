bb5a8785e64bec03e704bbdb12defe2e3e291dcc
Milan Petrović <millllan@gmail.com>
2018-10-29 16:56:21 +0100

    .

--- src/app/providers/auth.service.spec.ts
+++ src/app/providers/auth.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AuthService } from './user-data.service';
+import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
